e15aec220712e0c327e17ed812ede9c95c5b57db
Juri F <friindel@mail.ru>
2020-06-01 13:57:27 +0100

    lesson 34

--- public/app.js
+++ public/app.js
@@ -12,8 +12,57 @@ function something()
 function add_to_card(id)
 {
   var key = 'product_' + id; //extract variable
-  
+
   var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
+
+  update_orders_input();
+  update_orders_button();
+}
+
+function update_orders_input()
+{
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+  var text = 'Cart (' + cart_get_number_of_items() + ')';
+  $('#orders_button').val(text);
+}
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+
+  for(var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // get key
+    var value = window.localStorage.getItem(key); // get value; in ruby it looks like hh[key] = x
+
+    if (key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+  return cnt;
+}
+
+function cart_get_orders()
+{
+  var orders = '';
+
+  for(var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // get key
+    var value = window.localStorage.getItem(key); // get value; in ruby it looks like hh[key] = x
+
+    if (key.indexOf('product_') == 0)
+    {
+      orders = orders + key + '=' + value + ',';
+    }
+  }
+  return orders;
 }
